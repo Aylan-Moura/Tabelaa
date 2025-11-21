4a3061d567a8dc9a7f2d8c05c5f62a33315bc484
Aylan-Moura <mouraaylan@outlook.com>
2025-11-21 08:18:28 -0300

    p commit

--- src/Tabela.java
+++ src/Tabela.java
@@ -2,6 +2,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
+//@author Aylan
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
